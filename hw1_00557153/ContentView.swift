67c59b585f11d5a52dfd249bc10d7fffeb1136e3
tony <usengjake@gmail.com>
2021-10-20 17:15:06 +0800

    Initial Commit

---
+++ hw1_00557153/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hw1_00557153
+//
+//  Created by User01 on 2021/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hw1_00557153/hw1_00557153App.swift
@@ -0,0 +1,17 @@
+//
+//  hw1_00557153App.swift
+//  hw1_00557153
+//
+//  Created by User01 on 2021/10/20.
+//
+
+import SwiftUI
+
+@main
+struct hw1_00557153App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
